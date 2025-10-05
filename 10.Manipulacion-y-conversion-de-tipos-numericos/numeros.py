5170a67b7b814d8ce27f5b06faf3521704eb4e5c
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-10-05 16:26:08 -0400

    [FIRST COMMIT]

---
+++ 10.Manipulacion-y-conversion-de-tipos-numericos/numeros.py
@@ -0,0 +1,38 @@
+x = 1 # enteros
+y = 2.5 # flotantes
+z = 1j # imaginarios
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+positivo = 5.2
+negativo = -5.2
+
+imaginario = 5 +1j
+
+xf = float(x) # Conversion de tipo
+print(type(xf))
+print(xf)
+
+ye = int(y)
+print(type(ye))
+print(ye)
+
+
+entero = 5
+flotante = -5.5
+
+enteroComplejo = complex(entero)
+flotanteComplejo = complex(flotante)
+
+print(enteroComplejo)
+print(type(enteroComplejo))
+print(flotanteComplejo)
+print(flotanteComplejo)
+print(type(flotanteComplejo))
+print(flotanteComplejo)
+
+import random
+
+print(random.randrange(1, 10)) # Output random number beethwen 1 -9
---
+++ 9.Tipo-dato/tipo-dato.py
@@ -0,0 +1,36 @@
+comillasSimples = 'Este es un texto'
+comillasDobles = "Este es un texto"
+comillasTriples = '''Este es un texto'''
+
+print(comillasSimples)
+print(comillasDobles)
+print(comillasTriples)
+
+# Números
+a = 1
+b = 3.14
+c = 5 + 2j
+
+print(a)
+print(b)
+print(c)
+
+# Listas (Mutables)
+lista = [0,1,2,3,4,5]
+
+# Tuplas (Inmutables)
+tupla = ("a", "b", "c")
+
+# Diccionarios
+diccionario = {
+    "nombre"    :   "Dave",
+    "edad"      :   34,
+    "ciudado"   :   "La Paz"
+}
+
+# Conjuntos (sets) -> los elementos repetidos no se muestran
+conjunto = {1,1,2,2,3,3}
+
+# Booleano
+booleanoVerdadero = True
+booleanoFalso = False
