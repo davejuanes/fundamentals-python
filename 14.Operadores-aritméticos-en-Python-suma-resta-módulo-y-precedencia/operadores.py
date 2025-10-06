3fbd91b02c406d3e25106fb60725313a9c31e002
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-10-05 20:57:45 -0400

    [ADD] CLASE 14

---
+++ 14.Operadores-aritméticos-en-Python-suma-resta-módulo-y-precedencia/operadores.py
@@ -0,0 +1,37 @@
+# Operadores aritméticos
+
+x = 5
+y = 10
+
+# Suma
+print("Suma", x + y)
+
+# Resta
+print("Resta", x - y)
+
+# Multiplicación
+print("Multiplicación", x * y)
+
+# División (Siempre retorna un float)
+print("División", y / x)
+
+# Módulo %
+print("Módulo(resto)", y % x)
+
+# Potencia **
+print("Pontencia", y ** x)
+
+# División entera //
+print("División Entera", y // x) # No da valores decimales
+
+par = 8
+print(par % 2 == 0)
+
+# Precedencia de Operadores
+
+# Paréntesis
+# Exponentes
+# Multiplicaciones, Divisiones, Divisiones Enteras y Restos
+# Sumas y Restas
+# Comparaciones de Identidad y Pertenencia
+# Lógicos
