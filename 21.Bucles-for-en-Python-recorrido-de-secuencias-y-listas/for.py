ed64029010ed8ed143313921738e9282ea997852
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-10-06 00:16:02 -0400

    [ADD] CLASE 21

---
+++ 21.Bucles-for-en-Python-recorrido-de-secuencias-y-listas/for.py
@@ -0,0 +1,38 @@
+palabra = "Python"
+
+for letra in palabra:
+    print(letra)
+
+adjetivos = ["Rica", "Saludable"]
+frutas = ["Manzana", "Naranja", "Kiwi"]
+
+i = 0
+for fruta in frutas:
+    for adjetivo in adjetivos:
+            print(fruta, adjetivo)
+
+
+
+# for fruta in frutas:
+#     if fruta == "Naranja":
+#         continue
+#     print(fruta)
+# else:
+#     print("Ya hemos terminado el buble for")
+
+print("-----------------------------")
+
+# Range
+
+# Comienza desee el cero y termina en el número en el queasignemos sin incluilo
+# for i in range(10):
+#     print(i)
+
+# for i in range(3, 5):
+#     print(i)
+
+# for i in range(0, 10, 2): # Desde, hasta, incrementando
+#     print(i)
+
+for i in range(10):
+    pass
