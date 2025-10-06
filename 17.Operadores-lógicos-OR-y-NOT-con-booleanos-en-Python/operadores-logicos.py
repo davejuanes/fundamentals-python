76beda66b0fae61ab113c9a3cedca88107c9e3ab
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-10-05 21:34:06 -0400

    [ADD] CLASE 17

---
+++ 17.Operadores-lógicos-OR-y-NOT-con-booleanos-en-Python/operadores-logicos.py
@@ -0,0 +1,21 @@
+x = 5
+y = 3
+z = 5
+
+print(x == y) # Si x es igual? False
+print(x != y) # Si x es distinto? True
+print(x > y) # Si x es mayor? True
+print(x < y) # Si x es menor? False
+print(x >= z) # Si x es mayor o igual? True
+print(x <= y) # Si x es menor o igual? False
+
+# Operadores lógicos
+
+#       True    False
+print(x > y and y > z) # False
+print(x > y or y > z) # True
+
+v = True
+f = False
+print(not v)
+print(not f)
