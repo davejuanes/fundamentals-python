c59b709b6d3c30dbc3a6c9d996d50729d2b11b1f
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-10-05 23:55:21 -0400

    [ADD] CLASE 19

---
+++ 19.Sentencia-match-en-Python-3.10-para-control-de-flujo/match.py
@@ -0,0 +1,19 @@
+dia = 1
+
+match dia:
+    case 1:
+        print("Hoy es lunes")
+    case 2:
+        print("Hoy es martes")
+    case 3:
+        print("Hoy es miercoles")
+    case 4:
+        print("Hoy es jueves")
+    case 5:
+        print("Hoy es vieres")
+    case 6:
+        print("Hoy es sabado")
+    case 7:
+        print("Hoy es domingo")
+    case _:
+        print("No coincide con ninguna de las anteriores opciones")
