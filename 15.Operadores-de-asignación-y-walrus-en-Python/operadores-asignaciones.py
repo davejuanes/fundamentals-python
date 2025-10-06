ad6031b41f54bac495c415c91eafa1626136822d
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-10-05 21:16:24 -0400

    [ADD] CLASE 16

---
+++ 15.Operadores-de-asignación-y-walrus-en-Python/operadores-asignaciones.py
@@ -0,0 +1,16 @@
+x = 5
+
+x += 3 # Suma
+x -= 3 # Resta
+x *= 3 # Multiplicación
+x /= 3 # División
+x %= 2 # Módulo(resto)
+
+y = 20
+
+y //= 2 # División Entera
+y **= 3 # Exponente
+
+# WALRUS (Morsa) :=
+print(z := 3)
+print(z)
