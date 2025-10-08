e8030f1ccdb3e970f54ea38fa900063f4deb7728
David Gonzalo Mercado Juanes <dmercado@abc.gob.bo>
2025-10-07 22:06:21 -0400

    [ADD] CLASE 24

---
+++ 24.Conjuntos-en-Python-creación-métodos-y-operaciones-básicas/conjuntos.py
@@ -0,0 +1,53 @@
+# Conjunto (set): Colección no ordenada de elementos únicos (no se puede acceder por indice)
+
+frutas = ["Manzana", "Naranja", "Mandarina", "Naranja"]
+print(frutas)
+print(type(frutas))
+print(len(frutas))
+
+print("Manzana" in frutas)
+print("Pera" not in frutas)
+
+# Agregar
+# Add
+frutas.add("Pera")
+print(frutas)
+
+# Update
+frutasTropicales = {"Piña", "Mango"} # Agregar lstas, tuplas, conjuntos
+frutas.update(frutasTropicales)
+
+conjuntos = {"Python", 156, True}
+print(conjuntos)
+print(type(conjuntos))
+
+for item in conjuntos:
+    print(item)
+
+# Eliminarlos
+frutas.remove("Mango")
+print(frutas)
+
+# Discard
+frutas.discard("Pera")
+print(frutas)
+# Pop
+frutas.pop()
+print(frutas)
+# Clear
+frutas.clear()
+print(frutas)
+
+print("-----------------")
+
+a = {"a", "b", "c"}
+b = {"c", "d", "e"}
+
+c = a.union(b)
+print(c)
+
+i = a.intersection(b)
+print(i)
+
+d = a.difference(b)
+print(d)
